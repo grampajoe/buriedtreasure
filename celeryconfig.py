8342ba19badcef222ffbc59db7935f56af405467
Joe Friedl <joe@joefriedl.net>
2014-06-11 18:33:39 -0400

    Add a dedicated queue for the fetch_detail task

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,16 +1,35 @@
 from datetime import timedelta
 import os
 
+from kombu import Queue
+
 BROKER_URL = os.environ.get('RABBITMQ_BIGWIG_URL')
+
+CELERY_DEFAULT_QUEUE = 'default'
+CELERY_DEFAULT_EXCHANGE = 'tasks'
+CELERY_DEFAULT_EXCHANGE_TYPE = 'topic'
+CELERY_DEFAULT_ROUTING_KEY = 'default'
+
+CELERY_QUEUES = (
+    Queue('default', routing_key='default'),
+    Queue('fetch_detail', routing_key='fetch_detail'),
+)
+
+CELERY_ROUTES = {
+    'tasks.fetch_detail': {
+        'queue': 'fetch_detail',
+        'routing_key': 'fetch_detail',
+    },
+}
 
 CELERYBEAT_SCHEDULE = {
     'runs-every-5-minutes': {
         'task': 'tasks.fetch_listings',
-        'schedule': timedelta(minutes=5),
+        'schedule': timedelta(seconds=5),
     },
     'process-listings-every-5-minutes': {
         'task': 'tasks.process_listings',
-        'schedule': timedelta(minutes=5),
+        'schedule': timedelta(seconds=5),
     }
 }
 
